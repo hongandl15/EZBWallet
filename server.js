e00a327c6560380fbb69ca5b9193f68d4eef4dcf
An Nguyen <cuvitdet15@gmail.com>
2023-03-30 03:39:42 +0700

    fixbug

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require('express')
 const mongoose = require('mongoose')
 const User = require('./models/user.js')
+const express = require('express')
+const hbs = require('express-handlebars')
+const session = require('express-session')
 const app = express()
+var path = require('path')
+const port = process.env.PORT || 8080
+const wallet = require('./controllers/wallet.js')
+const login = require('./controllers/login.js')
+const register = require('./controllers/register.js')
+const admin = require('./controllers/admin.js')
+const user = require('./controllers/user.js')
 // mongoose.connect('mongodb://localhost:27017/e-wallet', function (err) {
 //     if (err) throw err;
 //     console.log('Kết nối database thành công');
@@ -48,5 +57,28 @@ User.find(function(err, users){
     }).save();
 
 })
+
+
+
+require('./server.js')
+app.engine('handlebars', hbs.engine({defaultLayout: 'main',}))
+app.use(express.urlencoded())
+app.use(session({ secret: 'fafsdhalj' }))
+app.set('view engine', 'handlebars')
+app.use(express.static(path.join(__dirname, './public')));
+
+app.use(function(req, res, next) { // không hiển thị lại message session khi back lại trang
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    next();
+});
+
+app.use('/', login)
+app.use('/', user)
+app.use('/', register)
+app.use('/', wallet)
+app.use('/', admin)
+
+
+app.listen(port, () => console.log(`Express started on https:/localhost:${port};`))
 
 // app.listen(port, () => console.log(`Express started on https:/localhost:${port};`))
